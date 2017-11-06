1d49f2af4c7b80edcb42eba6b9c53a33c343c668
michel <laurentmichelst@gmail.com>
2017-11-06 18:06:56 +0100

    modification de l'édit text du mainactivity

--- app/src/main/java/com/michelapplication/moodtracker/MainActivity.java
+++ app/src/main/java/com/michelapplication/moodtracker/MainActivity.java
@@ -1,6 +1,8 @@
 package com.michelapplication.moodtracker;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -19,10 +21,15 @@ public class MainActivity extends AppCompatActivity {
     private MpagerAdapter mpagerAdapter;
     private Button btn_history;
     //rajout carré blanc, edit text, et btn annule et valide
+    protected Button btn_commentaire;
     protected Button btn_annuler_commentaire;
     protected Button btn_valider_commentaire;
     protected EditText edit_text_commentaire;
     protected TextView carre_blanc;
+    //SharedPreferences
+    private SharedPreferences mSharedPreferences;
+    protected static final String MONHUMEUR = "MonHumeur";
+    protected static final String HUM = "Hum";
 
 
     @Override
@@ -40,11 +47,13 @@ public class MainActivity extends AppCompatActivity {
         btn_annuler_commentaire.setVisibility(View.INVISIBLE);
         btn_valider_commentaire = (Button) findViewById(R.id.btn_valider_commentaire);
         btn_valider_commentaire.setVisibility(View.INVISIBLE);
+        btn_commentaire = (Button) findViewById(R.id.button_commenter);
         edit_text_commentaire = (EditText) findViewById(R.id.edit_text);
         edit_text_commentaire.setVisibility(View.INVISIBLE);
         carre_blanc = (TextView) findViewById(R.id.carre_blanc);
         carre_blanc.setVisibility(View.INVISIBLE);
-
+        //SharedPreferences
+        mSharedPreferences = getSharedPreferences(MONHUMEUR, Context.MODE_PRIVATE);
 
 
         btn_history.setOnClickListener(new View.OnClickListener() {
@@ -56,55 +65,48 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        btn_annuler_commentaire.setOnClickListener(new View.OnClickListener() {
+        btn_commentaire.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                //affichage de la fenetre edit text en mode visible
-                btn_annuler_commentaire = (Button) findViewById(R.id.btn_annuler_commentaire);
-                btn_annuler_commentaire.setVisibility(View.VISIBLE);
-                btn_valider_commentaire = (Button) findViewById(R.id.btn_valider_commentaire);
-                btn_valider_commentaire.setVisibility(View.VISIBLE);
-                edit_text_commentaire = (EditText) findViewById(R.id.edit_text);
-                edit_text_commentaire.setVisibility(View.VISIBLE);
-                carre_blanc = (TextView) findViewById(R.id.carre_blanc);
-                carre_blanc.setVisibility(View.VISIBLE);
+                HideShow(true);
             }
         });
 
         btn_valider_commentaire.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                HideShow(false);
+                //SharedPreferences
+                String h = edit_text_commentaire.getText().toString();
+                SharedPreferences.Editor editor = mSharedPreferences.edit();
+                editor.putString(HUM,h);
+                editor.commit();
 
-                //recupération edit text et envoi sur la page resultat
-                Intent intent = new Intent(getApplicationContext(),PageResult.class);
-                intent.putExtra("name",edit_text_commentaire.getText().toString());
-                startActivity(intent);
-
-                // fenetre edit text invisible
-                btn_annuler_commentaire = (Button) findViewById(R.id.btn_annuler_commentaire);
-                btn_annuler_commentaire.setVisibility(View.INVISIBLE);
-                btn_valider_commentaire = (Button) findViewById(R.id.btn_valider_commentaire);
-                btn_valider_commentaire.setVisibility(View.INVISIBLE);
-                edit_text_commentaire = (EditText) findViewById(R.id.edit_text);
-                edit_text_commentaire.setVisibility(View.INVISIBLE);
-                carre_blanc = (TextView) findViewById(R.id.carre_blanc);
-                carre_blanc.setVisibility(View.INVISIBLE);
             }
         });
 
         btn_annuler_commentaire.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                btn_annuler_commentaire = (Button) findViewById(R.id.btn_annuler_commentaire);
-                btn_annuler_commentaire.setVisibility(View.INVISIBLE);
-                btn_valider_commentaire = (Button) findViewById(R.id.btn_valider_commentaire);
-                btn_valider_commentaire.setVisibility(View.INVISIBLE);
-                edit_text_commentaire = (EditText) findViewById(R.id.edit_text);
-                edit_text_commentaire.setVisibility(View.INVISIBLE);
-                carre_blanc = (TextView) findViewById(R.id.carre_blanc);
-                carre_blanc.setVisibility(View.INVISIBLE);
+               HideShow(false);
             }
         });
+    }
 
+    private void HideShow(boolean isVisible){
+        int codeVisible = 0;
+        if(isVisible == true){
+            codeVisible = View.VISIBLE;
+        }else{
+            codeVisible = View.INVISIBLE;
+        }
+        btn_annuler_commentaire = (Button) findViewById(R.id.btn_annuler_commentaire);
+        btn_annuler_commentaire.setVisibility(codeVisible);
+        btn_valider_commentaire = (Button) findViewById(R.id.btn_valider_commentaire);
+        btn_valider_commentaire.setVisibility(codeVisible);
+        edit_text_commentaire = (EditText) findViewById(R.id.edit_text);
+        edit_text_commentaire.setVisibility(codeVisible);
+        carre_blanc = (TextView) findViewById(R.id.carre_blanc);
+        carre_blanc.setVisibility(codeVisible);
     }
 }
