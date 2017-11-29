35f02528ce40a89d48c54a1d975fff4cccde309f
michel <laurentmichelst@gmail.com>
2017-11-29 10:59:38 +0100

    mise en place des textView et des bouttons pageResult

--- app/src/main/java/com/michelapplication/moodtracker/controller/PageResult.java
+++ app/src/main/java/com/michelapplication/moodtracker/controller/PageResult.java
@@ -2,14 +2,51 @@ package com.michelapplication.moodtracker.controller;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageButton;
+import android.widget.TextView;
 
 import com.michelapplication.moodtracker.R;
 
 public class PageResult extends AppCompatActivity {
 
+    //text view
+    private TextView mView7;
+    private TextView mView6;
+    private TextView mView5;
+    private TextView mView4;
+    private TextView mView3;
+    private TextView mView2;
+    private TextView mView1;
+    //image btn
+    private ImageButton btn7;
+    private ImageButton btn6;
+    private ImageButton btn5;
+    private ImageButton btn4;
+    private ImageButton btn3;
+    private ImageButton btn2;
+    private ImageButton btn1;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_page_result);
+
+        // text view and image btn
+        mView7 = (TextView) findViewById(R.id.a_week_ago);
+        mView6 = (TextView) findViewById(R.id.six_days_ago);
+        mView5 = (TextView) findViewById(R.id.five_days_ago);
+        mView4 = (TextView) findViewById(R.id.four_days_ago);
+        mView3 = (TextView) findViewById(R.id.three_days_ago);
+        mView2 = (TextView) findViewById(R.id.day_before_yesterday);
+        mView1 = (TextView) findViewById(R.id.yesterday);
+        btn7 = (ImageButton) findViewById(R.id.btn_7);
+        btn6 = (ImageButton) findViewById(R.id.btn_6);
+        btn5 = (ImageButton) findViewById(R.id.btn_5);
+        btn4 = (ImageButton) findViewById(R.id.btn_4);
+        btn3 = (ImageButton) findViewById(R.id.btn_3);
+        btn2 = (ImageButton) findViewById(R.id.btn_2);
+        btn1 = (ImageButton) findViewById(R.id.btn_1);
+
+        
     }
 }
